df29ba92182976f16a9f9f3e6b152ff9e9ab7370
Jan Slifka <jan.slifka@gmail.com>
2022-08-08 22:13:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# DSW Importer SDK
+
+[![Node.js Package CI](https://github.com/ds-wizard/dsw-importer-sdk/workflows/Node.js%20Package/badge.svg)](https://github.com/ds-wizard/dsw-importer-sdk/actions)
+[![npm version](https://badge.fury.io/js/@ds-wizard%2Fimporter-sdk.svg)](https://badge.fury.io/js/@ds-wizard%2Fimporter-sdk)
+[![License](https://img.shields.io/github/license/ds-wizard/dsw-importer-sdk)](LICENSE)
+
+
+## Instalation
+
+```bash
+$ npm install @ds-wizard/importer-sdk
+```
+
+You can then import the library:
+
+```javascript
+var DSWImporter = require('@ds-wizard/importer-sdk')
+```
+
+or using ES6:
+
+```javascript
+import DSWImporter from '@ds-wizard/importer-sdk'
+```
+
+Alternatively, you can just import the library using script tag and CDN:
+
+```html
+<script src="https://unpkg.com/@ds-wizard/importer-sdk/lib/index.js"></script>
+```
+
+`DSWImporter` will then become globally available.
+
+## Usage
+
+TBD
+
+## License
+
+This project is licensed under the Apache License v2.0 - see the
+[LICENSE](LICENSE) file for more details.
---
+++ src/index.js
@@ -0,0 +1,58 @@
+export default class DSWImporter {
+    constructor() {
+        this._events = []
+        this._origin = null
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            if (!window.opener) {
+                reject(new Error('window.opener not set'))
+                return
+            }
+
+            window.opener.postMessage({ type: 'ready' }, '*')
+            window.addEventListener('message', (event) => {
+                if (event.data.type === 'ready') {
+                    this._origin = event.origin
+                    resolve()
+                }
+            }, false)
+        })
+    }
+
+    setReply(path, value) {
+        this._events.push({
+            type: Array.isArray(value) ? 'ReplyList' : 'ReplyString',
+            path: path.join('.'),
+            value: value
+        })
+    }
+
+    addItem(path) {
+        const uuid = DSWImporter.createUUID()
+        this._events.push({
+            type: 'AddItem',
+            path: path.join('.'),
+            uuid: uuid
+        })
+        return uuid
+    }
+
+    send() {
+        window.opener.postMessage({
+            type: 'Import',
+            events: this._events
+        }, this._origin)
+    }
+
+    static createUUID() {
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (dt + Math.random() * 16) % 16 | 0;
+            dt = Math.floor(dt / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
+    }
+}
